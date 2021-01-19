c257e0bc82ee10d882b68e0d2f591275a5beeccd
andryush <andryush333@gmail.com>
2021-01-19 16:34:30 +0400

    added DefaultPoster

--- src/components/pages/MovieCredit/MovieCredit.jsx
+++ src/components/pages/MovieCredit/MovieCredit.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CallApi from "../../../api/api";
+import DefaultPoster from "../../../assets/images/poster-not-found.jpg";
 class MovieCredit extends React.Component {
   state = {
     movieCredits: [],
@@ -19,13 +20,13 @@ class MovieCredit extends React.Component {
 
   render() {
     const creditsCard = this.state.movieCredits.map((el) => {
+      const poster =
+        el.profile_path === null
+          ? DefaultPoster
+          : `https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${el.profile_path}`;
       return (
         <div className="card mt-3" style={{ width: "18rem" }} key={el.id}>
-          <img
-            className="card-img-top"
-            src={`https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${el.profile_path}`}
-            alt={el.name}
-          />
+          <img className="card-img-top" src={poster} alt={el.name} />
           <div className="card-body">
             <p className="card-text">{el.name}</p>
           </div>
